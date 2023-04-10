bd4247f1c5d0ef38d5917174bdc549d2aa134046
MdTarikulIslam <nayon1820@gmail.com>
2023-04-10 15:41:00 +0600

    some code

---
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -0,0 +1,61 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import ListGroup from "react-bootstrap/ListGroup";
+import brand1 from "../../../assets/brands/1.webp";
+import brand2 from "../../../assets/brands/2.png";
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaWhatsapp,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+import Carousel from "react-bootstrap/Carousel";
+
+const RightSideNav = () => {
+  return (
+    <div>
+      <ButtonGroup vertical>
+        <Button className="mb-4" variant="outline-primary">
+          {" "}
+          <FaGoogle></FaGoogle> Login with Google
+        </Button>
+        <Button variant="outline-dark">
+          {" "}
+          <FaGithub></FaGithub> Login with Github
+        </Button>
+      </ButtonGroup>
+      <div>
+        <h3 className="my-5">Find us on</h3>
+        <ListGroup>
+          <ListGroup.Item className="mb-3">
+            <FaFacebook></FaFacebook> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-3">
+            <FaWhatsapp></FaWhatsapp> WhatsApp
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-3">
+            <FaTwitter></FaTwitter> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-3">
+            <FaLinkedin></FaLinkedin> LinkedIn
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <div>
+        <Carousel>
+          <Carousel.Item>
+            <img className="d-block w-100" src={brand1} alt="First slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={brand2} alt="Second slide" />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default RightSideNav;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOWvk6WhRedUYJ7qPuzKTOTZDgUWWJkEk",
+  authDomain: "newspaper-e362f.firebaseapp.com",
+  projectId: "newspaper-e362f",
+  storageBucket: "newspaper-e362f.appspot.com",
+  messagingSenderId: "747731659835",
+  appId: "1:747731659835:web:4071e6a904fd9501c1001e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
