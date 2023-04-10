5f7d90efeabee5218126ba8dd88d90716ce6096e
MdTarikulIslam <nayon1820@gmail.com>
2023-04-10 16:00:19 +0600

    full info added

--- src/Pages/Category/Category/Category.js
+++ src/Pages/Category/Category/Category.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import NewsSummaryCard from "../../Shared/NewsSummaryCard/NewsSummaryCard";
 
 const Category = () => {
-  const news = useLoaderData();
+  const categoryNews = useLoaderData();
   return (
     <div>
-      <h2>I am category {news.length}</h2>
+      <h2>This category has {categoryNews.length} news</h2>
+      {categoryNews.map((news) => (
+        <NewsSummaryCard key={news._id} news={news}></NewsSummaryCard>
+      ))}
     </div>
   );
 };
